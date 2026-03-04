0e25b68d515cf5bd4355910659c6d382154f160e
Richard Beneš <richard.benes.st@vsb.cz>
2026-03-04 19:38:23 +0100

    Blink two LEDs at different frequencies using tasks

--- source/POSRC_semproj.cpp
+++ source/POSRC_semproj.cpp
@@ -46,7 +46,7 @@ int main(void) {
 
     gpio_pin_config_t outConfig = {
 		.pinDirection = kGPIO_DigitalOutput,
-		.outputLogic = 0U
+		.outputLogic = 1U /* Turn those LEDs off at startup */
     };
 
     GPIO_PinInit(GPIOD, 1U, &outConfig); /* Blue LED */
@@ -67,10 +67,28 @@ int main(void) {
 /* TODO: pass the LED color as an argument */
 void vGreenTask(void* param) {
 
+	TickType_t halfPeriod = 1500U;
+
+	for (;;) {
+		GPIO_WritePinOutput(GPIOD, 1U, 0U); /* Turn blue LED on */
+		vTaskDelay(halfPeriod);
+		GPIO_WritePinOutput(GPIOD, 1U, 1U); /* Turn blue LED off */
+		vTaskDelay(halfPeriod);
+	}
+
 	for (;;) {}
 }
 
 void vBlueTask(void* param) {
+
+	TickType_t halfPeriod = 50U;
+
+	for (;;) {
+		GPIO_WritePinOutput(GPIOB, 19U, 0U); /* Turn green LED on */
+		vTaskDelay(halfPeriod);
+		GPIO_WritePinOutput(GPIOB, 19U, 1U); /* Turn green LED off */
+		vTaskDelay(halfPeriod);
+	}
 
 	for (;;) {}
 }
